dad33edfad8c3cc7237d6738ec3ca0f609d2ee66
Matthew McLeod <m@qwrk.dev>
2026-06-20 18:46:02 +0200

    noises

--- godot/scripts/arachnoise.gd
+++ godot/scripts/arachnoise.gd
@@ -35,6 +35,9 @@ func _ready() -> void:
 	
 	pass # Replace with function body.
 
+func _input(event: InputEvent) -> void:
+	if event.is_action("refresh_random"):
+		%Pads.play()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
