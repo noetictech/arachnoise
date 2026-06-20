ecaeef1ab20aebbbce854cafc79ec9ce8a7c5c4c
Matthew McLeod <m@qwrk.dev>
2026-06-20 15:04:20 +0200

    state chart, basics

--- godot/scripts/anchor_webline.gd
+++ godot/scripts/anchor_webline.gd
@@ -4,9 +4,15 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	line_texture.texture = load("res://assets/line%dd-2.png" % randi_range(1,6))
+	_randomize()
 
+func _randomize() -> void:
+	line_texture.texture = load("res://assets/line%dd-2.png" % randi_range(1,6))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+func _input(event: InputEvent) -> void:
+	if event.is_action("refresh_random"):
+		_randomize()
