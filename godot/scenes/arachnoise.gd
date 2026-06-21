9134358819faedd4552cae24b8b0d5caf0ae26a7
Matthew McLeod <m@qwrk.dev>
2026-06-21 17:14:05 +0200

    sanitizing

--- godot/scenes/arachnoise.gd
+++ godot/scenes/arachnoise.gd
@@ -5,10 +5,8 @@ extends Node2D
 
 # Okay, we're vaguely setup. 
 
-# We're going to make use of our 'anchor' lines, 1-12,
+# We're going to make use of our 'anchor' lines, 1-13,
 # which are representative of the following -
-
-
 
 # 1 - C > C Dm Em F G Am B
 # 2 - G > G Am Bm C D Em F#
@@ -25,9 +23,7 @@ extends Node2D
 #13 - F > F Gm Am Bb C Dm E
 
 
-
-# This gives us a sequence to follow for each 'prey' - who will land
-# on some point in this space
+# This gives us a sequence to follow for each 'prey'
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -43,7 +39,35 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_waiting_state_entered() -> void:
+func _on_loitering_state_entered() -> void:
 	# Random timer from 4-7s, mosquito 'appears' - root note chosen, gradient radial blur with note
 	# color and pulse animation near random screen edge. 
 	pass
+
+
+func _on_enticing_state_entered() -> void:
+	pass # Replace with function body.
+
+
+func _on_trapping_state_entered() -> void:
+	pass # Replace with function body.
+
+
+func _on_ensnared_state_entered() -> void:
+	pass # Replace with function body.
+
+
+func _on_phrase_1_state_entered() -> void:
+	pass # Replace with function body.
+
+
+func _on_phrase_2_state_entered() -> void:
+	pass # Replace with function body.
+
+
+func _on_phrase_3_state_entered() -> void:
+	pass # Replace with function body.
+
+
+func _on_success_state_entered() -> void:
+	pass # Replace with function body.
