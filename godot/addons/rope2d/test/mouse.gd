532aaa6731c279b357fa21f70eefb078fde24317
Matthew McLeod <m@qwrk.dev>
2026-06-29 15:33:55 +0200

    deletions

--- godot/addons/rope2d/test/mouse.gd
+++
@@ -1,4 +0,0 @@
-extends StaticBody2D
-
-func _process(delta: float) -> void:
-	global_position = get_global_mouse_position()
--- godot/addons/rope2d/test/specific_length.gd
+++
@@ -1,4 +0,0 @@
-extends Label
-
-func _ready() -> void:
-	$Rope2D.create_rope($Target.global_position, 100)
